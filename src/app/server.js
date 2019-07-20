5e91445964c8e1cb61de5075efc8bedd2fde4327
Luis Velito <luisvelito@Luiss-MacBook-Pro.local>
2019-07-19 23:44:19 -0500

    try

--- src/app/server.js
+++ src/app/server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/datamanager'));
+app.use(express.static(__dirname + '/dist'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/datamanager/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // default Heroku PORT
